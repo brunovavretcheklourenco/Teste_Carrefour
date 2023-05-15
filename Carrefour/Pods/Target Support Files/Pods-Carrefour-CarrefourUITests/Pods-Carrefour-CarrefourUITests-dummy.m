325d1c6dfa6a266b43972bfd11b03ab88541273c
Bruno Vavretchek <blourenco.exactaworks@alelo.com.br>
2023-05-15 18:22:57 -0300

    Removing Garbage

---
+++ Carrefour/Pods/Target Support Files/Pods-Carrefour-CarrefourUITests/Pods-Carrefour-CarrefourUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Carrefour_CarrefourUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Carrefour_CarrefourUITests
+@end
---
+++ Carrefour/Pods/Target Support Files/Pods-Carrefour/Pods-Carrefour-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Carrefour : NSObject
+@end
+@implementation PodsDummy_Pods_Carrefour
+@end
