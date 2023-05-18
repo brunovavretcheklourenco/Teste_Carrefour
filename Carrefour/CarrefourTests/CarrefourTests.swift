8442d88257a94a2d7b2d0ea5282a3e10a5ab595e
Bruno Vavretchek <blourenco.exactaworks@alelo.com.br>
2023-05-18 13:18:37 -0300

    Adicionando Teste Unitário da classe UserDetailsViewModel e arquivo Readme

--- Carrefour/CarrefourTests/CarrefourTests.swift
+++ Carrefour/CarrefourTests/CarrefourTests.swift
@@ -8,29 +8,44 @@
 import XCTest
 @testable import Carrefour
 
-final class CarrefourTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class UserDetailsViewModelTests: XCTestCase {
+    
+    func testInitializationWithUser() {
+        let user = User(login: "bruno", id: 123, node_id: "node123", avatar_url: "https://example.com/avatar.jpg", gravatar_id: "gravatar123", url: "https://example.com/user", html_url: "https://example.com/user/profile", followers_url: "https://example.com/user/followers", following_url: "https://example.com/user/following", gists_url: "https://example.com/user/gists", starred_url: "https://example.com/user/starred", subscriptions_url: "https://example.com/user/subscriptions", organizations_url: "https://example.com/user/orgs", repos_url: "https://example.com/user/repos", events_url: "https://example.com/user/events", received_events_url: "https://example.com/user/received_events", type: "User", site_admin: false, name: "Bruno", bio: "Olá, sou o Bruno", followers: 100, following: 50)
+        let viewModel = UserDetailsViewModel(user: user)
+        
+        XCTAssertEqual(viewModel.nameText, "Bruno")
+        XCTAssertEqual(viewModel.avatar, "https://example.com/avatar.jpg")
+        XCTAssertEqual(viewModel.followers, 100)
+        XCTAssertEqual(viewModel.following, 50)
+        XCTAssertNotNil(viewModel.bioText)
+        XCTAssertEqual(viewModel.bioText?.string, "Bio: Olá, sou o Bruno")
+        XCTAssertEqual(viewModel.login, "bruno")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testInitializationWithNilUser() {
+        let viewModel = UserDetailsViewModel(user: User(login: nil, id: nil, node_id: nil, avatar_url: nil, gravatar_id: nil, url: nil, html_url: nil, followers_url: nil, following_url: nil, gists_url: nil, starred_url: nil, subscriptions_url: nil, organizations_url: nil, repos_url: nil, events_url: nil, received_events_url: nil, type: nil, site_admin: nil, name: nil, bio: nil, followers: nil, following: nil))
+        
+        XCTAssertEqual(viewModel.nameText, "")
+        XCTAssertNil(viewModel.avatar)
+        XCTAssertEqual(viewModel.followers, 0)
+        XCTAssertEqual(viewModel.following, 0)
+        XCTAssertNil(viewModel.bioText)
+        XCTAssertNil(viewModel.login)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
+//MARK: Injetar a camada de Networking para testar o download das imagens
+    
+//    func testLoadImageWithValidURL() {
+//        let expectation = XCTestExpectation(description: "LoadImage completion called")
+//        let user = User(login: "Bruno", id: 123, node_id: "node123", avatar_url: "https://example.com/avatar.jpg", gravatar_id: "gravatar123", url: "https://example.com/user", html_url: "https://example.com/user/profile", followers_url: "https://example.com/user/followers", following_url: "https://example.com/user/following", gists_url: "https://example.com/user/gists", starred_url: "https://example.com/user/starred", subscriptions_url: "https://example.com/user/subscriptions", organizations_url: "https://example.com/user/orgs", repos_url: "https://example.com/user/repos", events_url: "https://example.com/user/events", received_events_url: "https://example.com/user/received_events", type: "User", site_admin: false, name: "Bruno", bio: "Olá, sou o Bruno", followers: 100, following: 50)
+//        let viewModel = UserDetailsViewModel(user: user)
+//
+//        viewModel.loadImage { image in
+//            XCTAssertNotNil(image)
+//            expectation.fulfill()
+//        }
+//
+//        wait(for: [expectation], timeout: 5.0)
+//    }
 }
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Teste_Carrefour
 Aplicativo Teste Carrefour
+
+A proposta principal do desafio está feita. 
+Contudo, sabemos que para o projeto ficar impecável leva tempo. 
+
+OBS: Não sou Designer, desculpe a UI rsrs
+
+
+
+TODO:
+
+- Colocar todas as navegações no pattern de coordinator
+
+- Tratar erros de API para informar ao Usuário mensagens relevantes
+
+- Adicionar mais testes unitários e também testes de UI
+
+- Textos em arquivos de string
+
+- Modularização de algumas camadas: Networking, Constraints, UI, Regras de negócio...
+
